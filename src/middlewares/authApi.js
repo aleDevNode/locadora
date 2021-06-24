b114434edecd7dd279cc828b6f3bbb9fbef39554
gideon <gideonam@yahoo.com.br>
2021-06-24 02:06:13 -0300

    :lock: create Middleware

---
+++ src/middlewares/authApi.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+const jwtPass = process.env.JWT_PASS
+
+const authApi = {
+    auth: async(req, res, next) => {
+        const authToken = req.headers["authorization"]
+        if(authToken != undefined) {
+            const bearer = authToken.split(" ")
+            const token = bearer[1]
+
+            try {
+                const user = await jwt.verify(token, jwtPass)
+                if(!user) return res.status(401).json({msg: "token invalid"})
+                req.token = token
+                req.user = user
+                next()
+            } catch (error) {
+                return res.status(401).json({msg: "token invalid"})
+            }
+        } else {
+                return res.status(401).json({msg: "token invalid"})
+
+        }
+    }
+}
+
+module.exports = authApi
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const usersController = require('./controllers/userController');
 const authController = require('./controllers/authController');
+const authApi = require('./middlewares/authApi');
+
+
 const routes = express.Router();
 
 // routes auth
@@ -8,9 +11,10 @@ const routes = express.Router();
 routes.post('/auth',authController.auth);
 
 // routes user
-routes.get('/users', usersController.index);
-routes.post('/users', usersController.create);
-routes.put('/users', usersController.update);
-routes.delete('/users', usersController.delete);
+routes.get('/users',authApi.auth, usersController.index);
+routes.post('/users',authApi.auth, usersController.create);
+routes.put('/users',authApi.auth, usersController.update);
+routes.delete('/users',authApi.auth, usersController.delete);
+
 
 module.exports = routes;
